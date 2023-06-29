40933b2cd66b86d42c0089c34634c6857371feaf
yuridapaz <yuridapaz@gmail.com>
2023-06-29 19:06:34 -0300

    first structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,5 @@
-import { useState } from 'react';
-
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <h1>Hello World!</h1>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
