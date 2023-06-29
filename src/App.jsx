ce3de42b72b713b1450bd20260d25102612867bc
yuridapaz <yuridapaz@gmail.com>
2023-06-29 19:32:31 -0300

    header-first-ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className='w-full h-screen bg-slate-200 flex flex-col'>
+        <Header />
+        <div>
+          <div>
+            <Navbar />
+          </div>
+          <div>Container</div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
