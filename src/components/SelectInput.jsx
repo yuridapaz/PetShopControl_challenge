8f76728452478a319d45ad3d80540387ba2307c0
yuridapaz <yuridapaz@gmail.com>
2023-07-24 16:56:12 -0300

    new

--- src/components/SelectInput.jsx
+++ src/components/SelectInput.jsx
@@ -1,18 +1,13 @@
 const SelectInput = ({ labelName, defaultValue, values }) => {
-  const valor = 'batatá doce ê';
-  console.log(valor.normalize('NFD').replace(/[\u0300-\u036f]/g, ''));
-  console.log('first');
   return (
     <div>
-      <label
-        htmlFor='tipo'
-        className='mb-1 ml-1 block text-xs font-medium text-gray-900 dark:text-zinc-50 '
-      >
+      <label htmlFor='tipo' className='mb-1 ml-1 block text-xs text-gray-900 dark:text-zinc-50 '>
         {labelName}
       </label>
       <select
         id='tipo'
-        className='block w-full rounded-sm border border-gray-300 bg-gray-50 p-0.5 text-xs text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-zinc-50 dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500'
+        className='block w-full rounded-sm  border border-gray-300 bg-gray-50 p-0.5 text-xs text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-zinc-50
+          dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 '
       >
         <option defaultValue> {defaultValue} </option>
         {values.map((value, i) => {
--- src/pages/DisplayPage.jsx
+++ src/pages/DisplayPage.jsx
@@ -1,5 +1,53 @@
+import SelectInput from '../components/SelectInput';
+
 const DisplayPage = () => {
-  return <div>DisplayPage</div>;
+  return (
+    <div className='pt-4'>
+      {/* Input's  */}
+      <div>
+        {/* Text Input + Order Filter */}
+        <div>
+          <div>
+            <input type='text' />
+          </div>
+          <SelectInput
+            defaultValue={'ordenar'}
+            labelName={'Ordenar'}
+            values={['A-Z', 'Z-A', 'Pequeno-Grande', 'Grande-Pequeno']}
+          />
+          {/* <div>
+            <label htmlFor='ordenar'></label>
+            <select name='ordenar'>
+              <option value='a-z'>A-Z</option>
+              <option value='z-a'>Z-A</option>
+              <option value='size-up'>Micro-Grande</option>
+              <option value='size-down'>Grande-Micro</option>
+            </select>
+          </div> */}
+        </div>
+      </div>
+      {/* Select's  */}
+      <div>
+        <SelectInput
+          defaultValue={'tipo'}
+          labelName={'Tipo'}
+          values={['Cachorro', 'Gato', 'Pássaro']}
+        />
+        <SelectInput
+          defaultValue={'raça'}
+          labelName={'Raça'}
+          values={['Schnauzer', 'SRD', 'Outro']}
+        />
+        <SelectInput
+          defaultValue={'porte'}
+          labelName={'Porte'}
+          values={['Micro', 'Pequeno', 'Médio', 'Grande']}
+        />
+      </div>
+      {/* Display Cards Grid */}
+      <div></div>
+    </div>
+  );
 };
 
 export default DisplayPage;
