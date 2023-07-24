d9b102ba6472c56be3c01ba5d40807701ff1d7c6
yuridapaz <yuridapaz@gmail.com>
2023-07-24 20:21:55 -0300

    new-layout

--- src/components/SelectInput.jsx
+++ src/components/SelectInput.jsx
@@ -1,12 +1,21 @@
-const SelectInput = ({ labelName, defaultValue, values }) => {
+const SelectInput = ({ labelName, defaultValue, values, hideLabel }) => {
   return (
-    <div>
-      <label htmlFor='tipo' className='mb-1 ml-1 block text-xs text-gray-900 dark:text-zinc-50 '>
+    <div
+      className={`flex w-full flex-col gap-0.5 @3xl:max-w-[150px]   ${
+        hideLabel ? 'w-auto self-stretch  ' : ''
+      }`}
+    >
+      <label
+        htmlFor='tipo'
+        className={`ml-0.5 text-xs text-gray-900 dark:text-zinc-50 ${
+          hideLabel ? 'hidden @3xl:block ' : ''
+        } `}
+      >
         {labelName}
       </label>
       <select
         id='tipo'
-        className='block w-full rounded-sm  border border-gray-300 bg-gray-50 p-0.5 text-xs text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-zinc-50
+        className='h-full w-full rounded-sm border  border-gray-300  bg-gray-50 p-0.5 pr-0  text-xs text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-zinc-50
           dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 '
       >
         <option defaultValue> {defaultValue} </option>
--- src/pages/DisplayPage.jsx
+++ src/pages/DisplayPage.jsx
@@ -2,47 +2,44 @@ import SelectInput from '../components/SelectInput';
 
 const DisplayPage = () => {
   return (
-    <div className='pt-4'>
+    <div className='w-full @container'>
       {/* Input's  */}
-      <div>
+      <div className='flex flex-col gap-4 p-4 @3xl:flex-row @3xl:border @3xl:border-slate-50'>
         {/* Text Input + Order Filter */}
-        <div>
-          <div>
-            <input type='text' />
+        <div className='flex gap-2 @3xl:w-full @3xl:gap-4'>
+          <div className='h-full w-full'>
+            <input
+              type='text'
+              placeholder='Pesquisar...'
+              className='h-full w-full rounded-sm  border border-gray-300 bg-gray-50 p-1.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500   dark:border-gray-600 dark:bg-gray-700 dark:text-zinc-50 dark:placeholder-zinc-100 dark:focus:border-blue-500 dark:focus:ring-blue-500'
+            />
           </div>
+
           <SelectInput
-            defaultValue={'ordenar'}
+            defaultValue={'Ordenar'}
             labelName={'Ordenar'}
             values={['A-Z', 'Z-A', 'Pequeno-Grande', 'Grande-Pequeno']}
+            hideLabel
           />
-          {/* <div>
-            <label htmlFor='ordenar'></label>
-            <select name='ordenar'>
-              <option value='a-z'>A-Z</option>
-              <option value='z-a'>Z-A</option>
-              <option value='size-up'>Micro-Grande</option>
-              <option value='size-down'>Grande-Micro</option>
-            </select>
-          </div> */}
         </div>
-      </div>
-      {/* Select's  */}
-      <div>
-        <SelectInput
-          defaultValue={'tipo'}
-          labelName={'Tipo'}
-          values={['Cachorro', 'Gato', 'Pássaro']}
-        />
-        <SelectInput
-          defaultValue={'raça'}
-          labelName={'Raça'}
-          values={['Schnauzer', 'SRD', 'Outro']}
-        />
-        <SelectInput
-          defaultValue={'porte'}
-          labelName={'Porte'}
-          values={['Micro', 'Pequeno', 'Médio', 'Grande']}
-        />
+        {/* Select's  */}
+        <div className='flex w-full gap-2 @3xl:gap-4'>
+          <SelectInput
+            defaultValue={'Tipo'}
+            labelName={'Tipo'}
+            values={['Cachorro', 'Gato', 'Pássaro']}
+          />
+          <SelectInput
+            defaultValue={'Raça'}
+            labelName={'Raça'}
+            values={['Schnauzer', 'SRD', 'Outro']}
+          />
+          <SelectInput
+            defaultValue={'Porte'}
+            labelName={'Porte'}
+            values={['Micro', 'Pequeno', 'Médio', 'Grande']}
+          />
+        </div>
       </div>
       {/* Display Cards Grid */}
       <div></div>
