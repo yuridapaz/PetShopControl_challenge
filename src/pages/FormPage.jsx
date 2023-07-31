6ef26c78cd605ae6e45219c1b74de1080aceab7c
yuridapaz <yuridapaz@gmail.com>
2023-07-31 19:31:00 -0300

    handle-formPage

--- src/pages/FormPage.jsx
+++ src/pages/FormPage.jsx
@@ -2,8 +2,23 @@ import { TextInput } from '../components/TextInput';
 import { Button } from '../components/Button';
 import { SelectInput } from '../components/SelectInput';
 import { FaPaw } from 'react-icons/fa';
+import { useForm } from 'react-hook-form';
+import FormErrorMessage from '../components/FormErrorMessage';
 
 export const FormPage = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log('🚀 ~ file: FormPage.jsx:17 ~ onSubmit ~ data:', data);
+  };
+
+  console.log("🚀 ~ file: FormPage.jsx:19 ~ FormPage ~ watch('name'):", watch('name'));
+
   return (
     <div className='container flex h-full flex-1 flex-col items-center  p-4 '>
       <div className='mb-6 mt-2 flex self-center'>
@@ -14,58 +29,82 @@ export const FormPage = () => {
         <h1 className='text-xl'>Formulário de Cadastro</h1>
       </div>
       {/* Form */}
-      <form className='flex h-full w-full max-w-4xl flex-1 flex-col gap-4  '>
+      <form
+        className='flex h-full w-full max-w-4xl flex-1 flex-col gap-6'
+        onSubmit={handleSubmit(onSubmit)}
+      >
         {/* Name - Text Input */}
-        <div className='flex flex-col gap-1'>
+        <div className='relative flex flex-col gap-0.5'>
           <label htmlFor='name'> Nome: </label>
-          <TextInput id={'name'} />
+          <TextInput
+            id={'name'}
+            register={{ ...register('name', { required: true }) }}
+            error={errors.name && true}
+          />
+          {errors.name && <FormErrorMessage />}
         </div>
         {/* Tipo / Raça - Select */}
-        <div className='flex w-full flex-col gap-4'>
-          <div className='flex w-full flex-col gap-1'>
+        <div className='flex w-full flex-col gap-6'>
+          <div className='relative flex w-full flex-col gap-0.5'>
             <label htmlFor='tipo'>Tipo:</label>
             <SelectInput
-              defaultValue={'Escolher'}
               id={'tipo'}
               values={['Cachorro', 'Gato', 'Outro']}
+              register={{ ...register('tipo', { required: true }) }}
+              error={errors.tipo && true}
             />
+            {errors.tipo && <FormErrorMessage />}
           </div>
-          <div className='flex w-full flex-col gap-1'>
+          <div className='relative flex w-full flex-col gap-0.5'>
             <label htmlFor='raca'>Raça:</label>
-            <SelectInput defaultValue={'Escolher'} id={'raca'} values={['1', '2', '3']} />
+            <SelectInput
+              id={'raca'}
+              values={['1', '2', '3']}
+              register={{ ...register('raca', { required: true }) }}
+              error={errors.raca && true}
+            />
+            {errors.raca && <FormErrorMessage />}
           </div>
         </div>
         {/* Tamanho / Cor / Idade - Select  */}
-        <div className='flex w-full gap-4'>
-          <div className='flex w-full flex-col gap-1'>
+        <div className='flex w-full gap-6'>
+          <div className='relative flex w-full flex-col gap-0.5'>
             <label htmlFor='cor'>Cor:</label>
             <SelectInput
-              defaultValue={'Escolher'}
               id={'cor'}
               values={['cor 1 ', 'cor 2', 'cor 3']}
+              register={{ ...register('cor', { required: true }) }}
+              error={errors.cor && true}
             />
+            {errors.cor && <FormErrorMessage />}
           </div>
-          <div className='flex flex-col gap-1'>
+          <div className='relative flex flex-col gap-0.5'>
             <label htmlFor='idade'>Idade:</label>
-            {/* <SelectInput defaultValue={'Idade'} id={'idade'} values={['1', '2', '3']} /> */}
+            {/* <SelectInput ={'idade'} values={['1', '2', '3']} /> */}
             <input
               type='number'
               name='idade'
               max={20}
-              min={0}
+              min={1}
               id='idade'
-              className='w-32 rounded-sm border border-gray-300 bg-gray-50 px-3 py-2 text-sm text-gray-900 focus:border-blue-500  focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-zinc-50'
+              className={`w-32 rounded-md border border-gray-300 bg-gray-50 px-3 py-2 text-sm text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-zinc-50 ${
+                errors.idade && 'border-1 border-red-400 focus:outline-none'
+              }`}
+              {...register('idade', { required: true })}
             />
+            {errors.idade && <FormErrorMessage />}
           </div>
         </div>
         {/* Dono - Text Input / Firebase database select input */}
-        <div className='flex w-full flex-col gap-1'>
+        <div className='relative flex w-full flex-col gap-0.5'>
           <label htmlFor='tamanho'>Tamanho:</label>
           <SelectInput
-            defaultValue={'Escolher'}
             id={'tamanho'}
             values={['Micro', 'Pequeno', 'Médio', 'Grande', 'Gigante']}
+            register={{ ...register('tamanho', { required: true }) }}
+            error={errors.tamanho && true}
           />
+          {errors.tamanho && <FormErrorMessage />}
         </div>
 
         <Button type='submit' className={'mt-auto w-full max-w-xs self-center'}>
