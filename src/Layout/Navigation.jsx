2d4524b4d90a33e36e3a275c4ed79e4f03c00a58
yuridapaz <yuridapaz@gmail.com>
2023-08-24 11:13:06 -0300

    navigation new style

--- src/Layout/Navigation.jsx
+++ src/Layout/Navigation.jsx
@@ -12,55 +12,64 @@ const Navigation = () => {
   return (
     <>
       {/* Mobile Icon */}
-      <div className=" flex items-center justify-between bg-gray-300 shadow-md dark:bg-gray-700  md:hidden">
-        <a href="" className=" block p-3 font-light text-zinc-950 dark:text-zinc-50">
-          Pet Control
-        </a>
-        <button className="p-3 hover:bg-gray-500 dark:hover:bg-slate-700" onClick={toggleSideBar}>
-          <Bars3Icon className="h-8 w-8  text-zinc-950  active:text-zinc-50 dark:text-gray-50" />
-        </button>
+      <div className="p-2">
+        <div className="flex items-center justify-between rounded-xl bg-gray-300 shadow-md dark:bg-gray-700  md:hidden">
+          <a href="" className="block p-3 font-light  text-zinc-950 dark:text-zinc-50">
+            Pet Control
+          </a>
+          <button
+            className="p-0 hover:bg-gray-500 rounded-xl dark:hover:bg-slate-700"
+            onClick={toggleSideBar}
+          >
+            <Bars3Icon className="w-14 h-14 p-3 hover:text-white  dark:text-gray-50" />
+          </button>
+        </div>
       </div>
       {/* Sidebar */}
       <div
-        className={` absolute inset-y-0 left-0 z-10 flex w-8/12 sm:w-60 ${
+        className={`${
           openSideBar ? "" : "-translate-x-full"
-        } transform flex-col border-r-2 border-gray-400 bg-gray-100 p-4 transition duration-200 ease-in-out dark:border-gray-600 dark:bg-gray-900 md:relative md:translate-x-0`}
+        } h-screen absolute flex w-9/12 sm:w-1/2 md:w-72 lg:w-80 md:py-4  md:translate-x-0 lg:py-6 transition transform duration-200 ease-in-out md:relative inset-y-0 left-0 z-10 shrink-0 `}
       >
-        <div className="flex w-full flex-col items-center justify-between gap-2 border-b border-b-gray-400 py-4 dark:border-b-gray-600">
-          <div className="flex h-12 w-12 items-center">
-            <img src="/src/assets/images/user-image.png" alt="" />
+        <div
+          className={` flex-col flex shadow-lg bg-gray-100 p-6  dark:border-gray-600 dark:bg-gray-900 md:rounded-xl rounded-e-2xl border-r-2 border-r-gray-400 md:border-none w-full`}
+        >
+          <div className="flex w-full flex-col items-center justify-between gap-2 border-b border-b-gray-400 py-4 dark:border-b-gray-600">
+            <div className="flex h-12 w-12 items-center">
+              <img src="/src/assets/images/user-image.png" alt="" />
+            </div>
+            <h2 className="mt-2 text-center">Yuri da Paz Simonin</h2>
           </div>
-          <h2 className="mt-2 text-center">Yuri da Paz Simonin</h2>
-        </div>
-        <div>
-          <ul className="flex flex-col items-start gap-4  pt-10 ">
-            <Link to={"/"} onClick={toggleSideBar}>
-              <li className="underlineHover text-xl font-bold text-zinc-950 hover:cursor-pointer hover:text-zinc-800  dark:text-zinc-50  dark:hover:text-zinc-300">
-                Dashboard / Home
-              </li>
-            </Link>
-            <Link to={"/pets"} onClick={toggleSideBar}>
-              <li className="underlineHover text-xl font-bold text-zinc-950 hover:cursor-pointer  hover:text-zinc-800  dark:text-zinc-50 dark:hover:text-zinc-300">
-                Pet List
-              </li>
-            </Link>
-            <Link to={"/register"} onClick={toggleSideBar}>
-              <li className="underlineHover text-xl font-bold text-zinc-950 hover:cursor-pointer  hover:text-zinc-800  dark:text-zinc-50 dark:hover:text-zinc-300">
-                Cadastrar Pet
-              </li>
-            </Link>
-            {/* <Link to={'/'} onClick={toggleSideBar}>
-              <li className='underlineHover text-xl font-bold text-zinc-950 hover:cursor-pointer  hover:text-zinc-800  dark:text-zinc-50 dark:hover:text-zinc-300'>
-                Users
-              </li>
-            </Link> */}
-          </ul>
-        </div>
-        <div className="mt-auto flex justify-center">
-          <button className="flex items-center rounded-md bg-gray-400 px-6 py-2 text-xs  font-semibold text-white transition-all hover:bg-gray-500 dark:bg-gray-50 dark:text-zinc-900 dark:hover:bg-gray-200">
-            <ArrowRightOnRectangleIcon className="mr-2 h-5 w-5 text-zinc-200 dark:text-zinc-900" />
-            Logout
-          </button>
+          <div>
+            <ul className="flex flex-col items-start gap-4  pt-10 ">
+              <Link to={"/"} onClick={toggleSideBar}>
+                <li className="underlineHover text-xl font-bold text-zinc-950 hover:cursor-pointer hover:text-zinc-800  dark:text-zinc-50  dark:hover:text-zinc-300">
+                  Dashboard / Home
+                </li>
+              </Link>
+              <Link to={"/pets"} onClick={toggleSideBar}>
+                <li className="underlineHover text-xl font-bold text-zinc-950 hover:cursor-pointer  hover:text-zinc-800  dark:text-zinc-50 dark:hover:text-zinc-300">
+                  Pet List
+                </li>
+              </Link>
+              <Link to={"/register"} onClick={toggleSideBar}>
+                <li className="underlineHover text-xl font-bold text-zinc-950 hover:cursor-pointer  hover:text-zinc-800  dark:text-zinc-50 dark:hover:text-zinc-300">
+                  Cadastrar Pet
+                </li>
+              </Link>
+              {/* <Link to={'/'} onClick={toggleSideBar}>
+                <li className='underlineHover text-xl font-bold text-zinc-950 hover:cursor-pointer  hover:text-zinc-800  dark:text-zinc-50 dark:hover:text-zinc-300'>
+                  Users
+                </li>
+              </Link> */}
+            </ul>
+          </div>
+          <div className="mt-auto flex justify-center">
+            <button className="flex items-center rounded-md bg-gray-400 px-6 py-2 text-xs  font-semibold text-white transition-all hover:bg-gray-500 dark:bg-gray-50 dark:text-zinc-900 dark:hover:bg-gray-200">
+              <ArrowRightOnRectangleIcon className="mr-2 h-5 w-5 text-zinc-200 dark:text-zinc-900" />
+              Logout
+            </button>
+          </div>
         </div>
       </div>
     </>
--- src/pages/Pets/PetCard.jsx
+++ src/pages/Pets/PetCard.jsx
@@ -1,7 +1,22 @@
 const PetCard = ({ petInfo }) => {
   return (
-    <div className="w-full max-w-md shadow-md @container dark:text-zinc-200">
-      <div className="relative flex cursor-pointer items-center overflow-hidden rounded-sm ring-black/50 ring-offset-1 @container  hover:ring-1  dark:ring-zinc-100 dark:ring-offset-gray-800">
+    <li className="w-full my-1  grid grid-cols-2 md:grid-cols-4 sm:grid-cols-3 rounded-lg items-center justify-between hover:ring-1 dark:ring-zinc-100 dark:ring-offset-gray-800 ring-black/50 ring-offset-1 cursor-pointer p-1">
+      <div className="flex items-center gap-2">
+        <div className="w-10 h-10 shrink-0">
+          <img
+            src="src/assets/images/Todd.jpeg"
+            alt=""
+            className="rounded-full object-cover w-full h-full inline-block"
+          />
+        </div>
+        <p className=""> {petInfo.nome} </p>
+      </div>
+
+      <div className="">
+        <p className="text-gray-500"> {petInfo.raca} </p>
+      </div>
+
+      {/* <div className="relative flex cursor-pointer items-center overflow-hidden rounded-sm ring-black/50 ring-offset-1 @container  hover:ring-1  dark:ring-zinc-100 dark:ring-offset-gray-800">
         <img
           src="src/assets/images/Todd.jpeg"
           alt=""
@@ -10,24 +25,24 @@ const PetCard = ({ petInfo }) => {
 
         <div className="flex w-full flex-col items-start rounded-lg bg-gray-100 px-1 py-1 pl-16 dark:bg-gray-800  ">
           <div className="">
-            {/* <h1 className='text-sm'>Todd da Paz Simonin</h1> */}
+
             <h1 className="text-sm"> {petInfo.nome} </h1>
           </div>
           <div className="flex  gap-4 font-bold @xs:w-full @xs:flex-row  @xs:gap-4">
             <p className="text-xxs">
               Tipo:
-              {/* <span className='font-normal'> Cachorro </span> */}
+
               <span className="font-normal"> {petInfo.tipo} </span>
             </p>
             <p className="text-xxs ">
               Raça:
-              {/* <span className=' font-normal'> Schnauzer </span> */}
+
               <span className=" font-normal"> {petInfo.raca} </span>
             </p>
           </div>
         </div>
-      </div>
-    </div>
+      </div> */}
+    </li>
   );
 };
 
