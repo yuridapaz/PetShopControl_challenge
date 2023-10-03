00396f1e09416b78e4fb17e27c279666fe84fc4b
yuridapaz <yuridapaz@gmail.com>
2023-10-03 15:50:05 -0300

    Pet Card image

--- src/pages/Pets/PetCard.tsx
+++ src/pages/Pets/PetCard.tsx
@@ -9,11 +9,15 @@ const PetCard = ({ petData }: PetCardProps) => {
     <li className=" grid w-full cursor-pointer grid-cols-2 items-center justify-between rounded-sm p-1 ring-gray-400 ring-offset-1  hover:ring-1 @sm:grid-cols-2 @xl:grid-cols-3 @3xl:grid-cols-4 dark:ring-zinc-100/20 dark:ring-offset-gray-800">
       <div className="flex items-center gap-2 ">
         <div className="h-10 w-10 shrink-0">
-          <img
-            src="src/assets/images/Todd.jpeg"
-            alt=""
-            className="inline-block h-full w-full rounded-full object-cover"
-          />
+          {petData.imageURL ? (
+            <img src={petData.imageURL} alt="" className="inline-block h-full w-full rounded-full object-cover" />
+          ) : (
+            <img
+              src={`/src/assets/images/${petData.type}Avatar.png`}
+              alt=""
+              className="inline-block h-full w-full rounded-full object-cover"
+            />
+          )}
         </div>
         <p className="dark:text-gray-100 md:ml-2 md:text-lg"> {petData.name}</p>
       </div>
